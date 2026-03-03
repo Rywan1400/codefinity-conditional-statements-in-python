81c16267d05b65fe3a39e860fc0c0640fc8ccd62
Rywan1400 <>
2026-03-03 06:21:47 +0000

    Challenge: Working with Comparison Operators

--- conditional_statements_in_python/challenge_working_with_comparison_operators/main.py
+++ conditional_statements_in_python/challenge_working_with_comparison_operators/main.py
@@ -4,11 +4,11 @@ oranges_price = 25
 bananas_price = 15
 
 # Check if apples and bananas have the same cost
-___ = ___
+is_same_price = apples_price == bananas_price
 
 # Check if the combination of apples and oranges is cheaper than oranges and bananas
-___ = ___
-
+apples_price + oranges_price < oranges_price + bananas_price
+is_cheaper_combination = apples_price + oranges_price < oranges_price + bananas_price
 # Testing 
 print("The result of the first comparison:", is_same_price)
 print("The result of the second comparison:", is_cheaper_combination)
